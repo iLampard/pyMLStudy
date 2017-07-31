0334965177245a3ad5fa9fac103b1c0d76cd86f1
iLampard <siqiao_xue@163.com>
2017-07-31 16:25:04 +0800

    add some more em code

--- pyMLStudy/em/gmm_case.py
+++ pyMLStudy/em/gmm_case.py
@@ -3,6 +3,8 @@
 
 import numpy as np
 from scipy.stats import norm
+from scipy.stats import multivariate_normal
+from sklearn.mixture import GaussianMixture
 
 
 def e_step(mu, sigma, a, y):
@@ -35,13 +37,58 @@ def em(init_params, y, tol=10 ** -5):
     while np.max(np.abs(current_params - prev_params)) > tol:
         prev_params = current_params
         current_params = update_params(prev_params, y)
+        print current_params[0]
     return current_params
+
+
+def brutal_em(y):
+    num_iter = 100
+    n = len(y)
+    d = 1
+    # 随机指定
+    # mu1 = np.random.standard_normal(d)
+    # print mu1
+    # mu2 = np.random.standard_normal(d)
+    # print mu2
+    mu1 = np.min(y)
+    mu2 = np.max(y)
+    sigma1 = np.std(y)
+    sigma2 = np.std(y)
+    pi = 0.5
+    # EM
+    for i in range(num_iter):
+        # E Step
+        tau1 = pi * norm.pdf(y, mu1, sigma1)
+        tau2 = (1 - pi) * norm.pdf(y, mu2, sigma2)
+        gamma = tau1 / (tau1 + tau2)
+        # M Step
+        mu1 = np.dot(gamma, y) / np.sum(gamma)
+        mu2 = np.dot((1 - gamma), y) / np.sum((1 - gamma))
+        sigma1 = np.dot(gamma * (y - mu1).T, y - mu1) / np.sum(gamma)
+        sigma2 = np.dot((1 - gamma) * (y - mu2).T, y - mu2) / np.sum(1 - gamma)
+        pi = np.sum(gamma) / n
+        print i, ":\t", mu1, mu2
+    print '类别概率:\t', pi
+    print '均值:\t', mu1, mu2
+    print '方差:\n', sigma1, '\n\n', sigma2, '\n'
+
+
+def sklearn_em(y):
+    g = GaussianMixture(n_components=2, covariance_type='full', tol=1e-6, max_iter=1000)
+    print y_test.shape
+    g.fit(y_test[np.newaxis].T)
+    print '类别概率:\t', g.weights_[0]
+    print '均值:\n', g.means_, '\n'
+    print '方差:\n', g.covariances_, '\n'
+    mu1, mu2 = g.means_
+    sigma1, sigma2 = g.covariances_
+    return
 
 
 if __name__ == "__main__":
     y_test = np.array([-67, -48, 6, 8, 14, 16, 23, 24, 28, 29, 41, 49, 56, 60, 75])
     m = np.mean(y_test)
     v = np.std(y_test)
-    print m, v
-    init_params_test = ([m, m], [v, v], [0.6, 0.4])
-    print em(init_params_test, y_test)
+    init_params_test = ([np.min(y_test), np.max(y_test)], [v, v], [0.6, 0.4])
+    em(init_params_test, y_test)
+    brutal_em(y_test)
