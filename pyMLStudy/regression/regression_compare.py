2f3633c2f2b8111e8b9c21451700c38feffe9532
iLampard <siqiao_xue@163.com>
2018-02-05 18:02:05 +0800

    add regression_compare example

--- pyMLStudy/regression/regression_compare.py
+++ pyMLStudy/regression/regression_compare.py
@@ -10,15 +10,17 @@ from sklearn.exceptions import ConvergenceWarning
 import matplotlib as mpl
 import warnings
 
-
 warnings.filterwarnings(action='ignore', category=ConvergenceWarning)
+mpl.rcParams['font.sans-serif'] = [u'simHei']
+mpl.rcParams['axes.unicode_minus'] = False
+np.set_printoptions(suppress=True)
 
 
 def xss(y, y_hat):
     y = y.ravel()
     y_hat = y_hat.ravel()
     # Version 1
-    tss = ((y - np.average(y)) ** 2).sum()
+    tss = ((y - np.average(y)) ** 2).sum()  # total sum of squares
     rss = ((y_hat - y) ** 2).sum()
     ess = ((y_hat - np.average(y)) ** 2).sum()
     r2 = 1 - rss / tss
@@ -34,3 +36,42 @@ def xss(y, y_hat):
     # r2 = 1 - rss / tss
     corr_coef = np.corrcoef(y, y_hat)[0, 1]
     return r2, corr_coef
+
+
+if __name__ == '__main__':
+    N = 9
+    x = np.linspace(0, 6, N) + np.random.randn(N)
+    x = np.sort(x)
+    y = x ** 2 - 4 * x - 3 + np.random.randn(N)
+    x.shape = -1, 1
+    y.shape = -1, 1
+
+    models = [
+        Pipeline([
+            ('poly', PolynomialFeatures()),
+            ('linear', LinearRegression(fit_intercept=False))]),
+        Pipeline([
+            ('poly', PolynomialFeatures()),
+            ('linear', RidgeCV(alphas=np.logspace(-3, 2, 50), fit_intercept=False))]),
+        Pipeline([
+            ('poly', PolynomialFeatures()),
+            ('linear', LassoCV(alphas=np.logspace(-3, 2, 50), fit_intercept=False))]),
+        Pipeline([
+            ('poly', PolynomialFeatures()),
+            ('linear', ElasticNetCV(alphas=np.logspace(-3, 2, 50), l1_ratio=[.1, .5, .7, .9, .95, .99, 1],
+                                    fit_intercept=False))])
+    ]
+
+    plt.figure(figsize=(18, 12), facecolor='w')
+    d_pool = np.arange(1, N, 1)  # 多项式的阶数
+    m = d_pool.size
+    clrs = []  # 颜色
+    for c in np.linspace(16711680, 255, m):
+        clrs.append('#%06x' % c)
+
+    line_width = np.linspace(5, 2, m)
+    titles = u'线性回归', u'Ridge回归', u'LASSO', u'ElasticNet'
+    tss_list = []
+    rss_list = []
+    ess_list = []
+    ess_rss_list = []
